c45ff5d61f19381a012d858d61832007241a40b4
shiramalamud <shira.malamud@post.runi.ac.il>
2025-01-09 00:23:04 +0200

    hw8

--- User.java
+++ User.java
@@ -42,37 +42,127 @@
     }
 
     /** If this user follows the given name, returns true; otherwise returns false. */
-    public boolean follows(String name) {
-        //// Replace the following statement with your code
+    public boolean follows(String name) 
+    {
+        for(int i=0;i<getfCount();i++)
+        {
+            if(getfFollows()[i]!=null)
+            {
+                if(getfFollows()[i].toLowerCase().equals(name.toLowerCase()))
+                {
+                    return true;
+                }
+            }
+        }
+        
         return false;
     }
     /** Makes this user follow the given name. If successful, returns true. 
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
     public boolean addFollowee(String name) {
-        //// Replace the following statement with your code
-        return false;
+        if(maxfCount<=fCount)
+        {
+            return false;
+        }
+        for(int i=0; i<getfCount(); i++)
+        {
+            if(getfFollows()[i]!=null)
+            {
+                if(getfFollows()[i].toLowerCase().equals(name.toLowerCase()))
+                {
+                    return false;
+                }
+            }
+        }
+        follows[fCount]=name;
+        fCount++;
+        
+        return true;
     }
 
     /** Removes the given name from the follows list of this user. If successful, returns true.
      *  If the name is not in the list, does nothing and returns false. */
-    public boolean removeFollowee(String name) {
-        //// Replace the following statement with your code
+    public boolean removeFollowee(String name) 
+    {
+        for(int i=0; i<getfCount(); i++)
+        {
+            if(getfFollows()[i]!=null)
+            {
+                if(getfFollows()[i].toLowerCase().equals(name.toLowerCase()))
+                {
+                    for(int j=i; j<getfCount()-1; j++)
+                    {
+                        follows[j]=follows[j+1];
+                    }
+                    follows[fCount-1]=null;
+                    fCount--;
+                    return true;
+    
+                }
+            }
+        }
         return false;
+
     }
 
     /** Counts the number of users that both this user and the other user follow.
     /*  Notice: This is the size of the intersection of the two follows lists. */
-    public int countMutual(User other) {
-         //// Replace the following statement with your code
-        return 0;
+    public int countMutual(User other) 
+    {
+        int mutualcount=0;
+        for( int i=0; i<getfCount(); i++)
+        {
+            String namecheck=getfFollows()[i];
+            for(int j=0; j<other.getfCount(); j++)
+            {
+                if(getfFollows()[i]!=null&&other.getfFollows()[j]!=null)
+                {
+                    if(other.getfFollows()[j].toLowerCase().equals(namecheck.toLowerCase()))
+                    {
+                        mutualcount++;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return mutualcount;
     }
 
     /** Checks is this user is a friend of the other user.
      *  (if two users follow each other, they are said to be "friends.") */
-    public boolean isFriendOf(User other) {
-        //// Replace the following statement with your code
+    public boolean isFriendOf(User other) 
+    {
+        boolean isFriend1=false;
+        for(int i=0; i<getfCount(); i++)
+        {
+            if(this.getfFollows()[i]!=null)
+            {
+                if (this.getfFollows()[i].toLowerCase().equals(other.getName().toLowerCase())) {
+                    isFriend1 = true;
+                    break;
+                }
+            }
+
+        }
+        boolean isFriend2=false;
+        for (int i = 0; i < other.getfCount(); i++) {
+            if(other.getfFollows()[i]!=null)
+            {
+                if (other.getfFollows()[i].toLowerCase().equals(this.getName().toLowerCase())) {
+                    isFriend2 = true;
+                    break;
+                }
+            }
+        }
+        if(isFriend1&&isFriend2)
+        {
+            return true;
+        }
+        
         return false;
     }
+
     /** Returns this user's name, and the names that s/he follows. */
     public String toString() {
         String ans = name + " -> ";
